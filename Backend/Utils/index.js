46745c0a6b930b8eba31f49349519b09c937b397
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-13 23:18:35 -0300

    :sparkles: feat: Criação de categorias

---
+++ Backend/Utils/index.js
@@ -0,0 +1,22 @@
+const categoryGames = [
+  "MMORPG",
+  "Shooter",
+  "MMO",
+  "Social",
+  "Card Game",
+  "MOBA",
+  "Fighting",
+  "Strategy",
+  "Racing",
+  "Sports",
+  "Fantasy",
+  "MMORPG",
+  "Battle Royale",
+  "Action RPG",
+  "Card",
+  "ARPG",
+  "MMOARPG",
+];
+module.exports = {
+    categoryGames
+}
