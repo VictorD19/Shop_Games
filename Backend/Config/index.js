96a4acda83befe4623329dd5406fd48853ded15b
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-13 23:17:14 -0300

    :sparkles: feat: Configurando api MMO Games

---
+++ Backend/Config/index.js
@@ -0,0 +1,9 @@
+
+const config = {
+    headers: {
+        'X-RapidAPI-Key': process.env.APY_KEY_RAPID,
+        'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+      }
+}
+
+module.exports = config
