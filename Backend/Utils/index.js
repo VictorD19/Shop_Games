477565d7568e55739fe339fd19dc033493e7c215
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-14 23:32:32 -0300

    :sparkles: feat: Funções auxiliares

--- Backend/Utils/index.js
+++ Backend/Utils/index.js
@@ -20,7 +20,19 @@ const categoryGames = [
 const generatePagination = (array, page_size, page_number) => {
   return array.slice((page_number - 1) * page_size, page_number * page_size);
 };
+const getNumberRandom = (limit) => Math.floor(Math.random() * limit);
+const getItemsByArray = (list, amountItems) => {
+  const newList = [];
+  for (let i = 0; i < amountItems; i++) {
+    const random = getNumberRandom(list.length);
+    const game = list[random];
+    newList[i] = game;
+  }
+  return newList;
+};
 module.exports = {
-    categoryGames,
-    generatePagination
-}
+  categoryGames,
+  generatePagination,
+  getNumberRandom,
+  getItemsByArray,
+};
