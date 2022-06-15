a53db197e3b52386d4a1932054b81fd8d6839e38
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-14 23:30:45 -0300

    :sparkles: feat: Rotas para controllers

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { getAllGames, getGame, getNews, getCategoryOfGames } = require('../Controller/GamesController')
+const { getAllGames, getGame, getNews, getCategoryOfGames, getPopularGames, getNewGames } = require('../Controller/GamesController')
 const Routes = express.Router()
 
 Routes.get('/allgames',getAllGames)
 Routes.get('/game/:id',getGame)
 Routes.get('/news',getNews)
 Routes.get('/categorys',getCategoryOfGames)
+Routes.get('/populargames',getPopularGames)
+Routes.get('/recommendedgames',getRecommendedGames)
+Routes.get('/newgames',getNewGames)
 
 module.exports = Routes
