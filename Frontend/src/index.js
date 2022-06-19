9c31edcae92392582e3faae900fd11b714500130
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-19 15:44:52 -0300

    :wrench: Config styles

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./reset-css.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "react-bootstrap";
 
