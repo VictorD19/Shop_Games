4295e0d47caaf81bf203b52e65b59fd69975b34a
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-20 22:03:36 -0300

    :recycle: reafactor: Updated methor message toast

--- Frontend/src/Components/Toast/index.jsx
+++ Frontend/src/Components/Toast/index.jsx
@@ -1,17 +1,11 @@
 import { Toast } from "react-bootstrap";
 import { ToastContainerMain } from "./toash.styled";
 
-export const ToastNotification = ({ show = true, close,children, type }) => {
+export const ToastNotification = ({ show, close, message, type }) => {
   return (
-    <ToastContainerMain >
-      <Toast
-        onClose={close}
-        show={show}
-        delay={3000}
-        autohide
-        type={type}
-      >
-        <Toast.Body>{children}</Toast.Body>
+    <ToastContainerMain>
+      <Toast onClose={close} show={show} delay={3000} autohide type={type}>
+        <Toast.Body>{message}</Toast.Body>
       </Toast>
     </ToastContainerMain>
   );
