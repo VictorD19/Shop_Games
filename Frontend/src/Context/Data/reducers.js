121f176d4781765356383c76cbab568c6eaf94c2
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-21 15:46:16 -0300

    :sparkles: feat: User's initial reducer created

--- Frontend/src/Context/Data/reducers.js
+++ Frontend/src/Context/Data/reducers.js
@@ -1,12 +1,15 @@
+import { removeCookie } from "../../Utils/cookie";
 import { initialUserData } from "./initialState";
 
-export const reducerUser = (state,action)=>{
-    switch (action.method) {
-        case 'INITIAL_USER':
-            const data = action.data
-            return {...state, user: data}
-    
-        default:
-            return initialUserData;
-    }
-}   
+export const reducerUser = (state, action) => {
+  switch (action.method) {
+    case "INITIAL_USER":
+      const { email, _id, games } = action.data;
+      return { user: { email, _id }, my_games: games };
+    case "LOGOUT":
+      removeCookie("token");
+      return initialUserData;
+    default:
+      return initialUserData;
+  }
+};
