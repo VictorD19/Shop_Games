d507a910ea65dfc0369176ca8501b91a22f59876
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-20 21:03:07 -0300

    :sparkles: feat: Active Cors

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,13 @@ const app = express()
 const Routes = require('./Routes')
 const swagger =  require('swagger-ui-express')
 const swaggerJson = require('./swagger_output.json')
+const cors = require("cors");
 require('./DB/start.js');
 const port = 3001
 
 app.use(bodyP.json())
 app.use(bodyP.urlencoded({ extended:true }));
+app.use(cors())
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerJson));
 app.use('/',Routes)
 app.listen(port, () => console.log(`🚀 Api Iniciada na porta:  ${port}!`))
