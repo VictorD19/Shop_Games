3a125fbeb6390d12ec91a952f32cb68f9a984e06
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-22 21:42:16 -0300

    :sparkles: feat: RowList Games Carrousel

---
+++ Frontend/src/Components/RowCarrousel/index.jsx
@@ -0,0 +1,19 @@
+import Slider from "react-slick";
+import { RowList } from "./row.styled";
+
+export const RowCarrousel = ({ children, title }) => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+  };
+  return (
+    <RowList>
+      <h1> {title}</h1>
+      <Slider {...settings}>{children}</Slider>
+    </RowList>
+  );
+};
---
+++ Frontend/src/Components/RowCarrousel/row.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+export const RowList = styled.div`
+  margin: 1.5rem 0;
+  >h1 {
+    font-size: 1.25em;
+    font-weight: 600;
+    margin-bottom: 1.5rem;
+  }
+  .slick-prev,
+  .slick-next {
+    top: -1.5rem;
+  }
+  .slick-prev {
+    right: 2.3rem;
+}
+  .slick-next {
+    right: 0.8rem;
+  }
+`;
