018a15a4d27f1f0f2bfe20c36fb4f79310d99602
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-22 21:43:37 -0300

    :sparkles: feat: Populate Home

--- Frontend/src/Page/Home/index.jsx
+++ Frontend/src/Page/Home/index.jsx
@@ -1,9 +1,26 @@
 import { LayoutPage } from "../../Layout";
 import { CarrouselMain } from "../../Components/CarrouselMain";
+import { CardGame } from "../../Components/CardGame";
+import { RowCarrousel } from "../../Components/RowCarrousel";
 export const HomePage = () => {
   return (
     <LayoutPage>
       <CarrouselMain />
+      <RowCarrousel title="Recommended for you">
+        {[...Array(15)].map((game, idx) => (
+          <CardGame />
+        ))}
+      </RowCarrousel>
+      <RowCarrousel title="Popular games">
+        {[...Array(15)].map((game, idx) => (
+          <CardGame />
+        ))}
+      </RowCarrousel>
+      <RowCarrousel title="New games">
+        {[...Array(15)].map((game, idx) => (
+          <CardGame />
+        ))}
+      </RowCarrousel>
     </LayoutPage>
   );
 };
