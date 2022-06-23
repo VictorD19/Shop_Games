c30d2335690892b5ec312aea4be5deb55e0e50fa
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-22 21:42:59 -0300

    :sparkles: feat: Evaluation component

---
+++ Frontend/src/Components/ScoreGame/index.jsx
@@ -0,0 +1,13 @@
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { getNumberRandom } from "../../Utils";
+export const GetScoreGame = () => {
+  const color = "#f8cb62";
+  const numberStarRandom = getNumberRandom(5);
+  return (
+    <div>
+      {[...Array(5)].map((_, index) =>
+        index <= numberStarRandom ? <AiFillStar color={color} /> : <AiOutlineStar />
+      )}
+    </div>
+  );
+};
