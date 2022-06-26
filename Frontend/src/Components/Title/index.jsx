0c0f0729e9bc787c5ce082367658bf2ce40d6d1d
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-26 12:25:53 -0300

    :sparkles: feat: Title component

---
+++ Frontend/src/Components/Title/index.jsx
@@ -0,0 +1,5 @@
+import { TitleGames } from "./title.styled"
+
+export const NewTitle = ({children,margin})=>{
+   return  <TitleGames margin={margin}>{children}</TitleGames>
+}
---
+++ Frontend/src/Components/Title/title.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const TitleGames = styled.h2`
+font-size: 1.25rem;
+font-weight: 600 ;
+margin-bottom: ${({margin})=> margin || '1rem'};
+`
