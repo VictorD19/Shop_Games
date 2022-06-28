60f062278d2bfc433ec0c9bf56edb5c59cd9da8e
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-06-28 17:04:23 -0300

    :ok_hand: style: Added slash to title

--- Frontend/src/Components/Title/title.styled.js
+++ Frontend/src/Components/Title/title.styled.js
@@ -4,4 +4,15 @@ export const TitleGames = styled.h2`
 font-size: 1.25rem;
 font-weight: 600 ;
 margin-bottom: ${({margin})=> margin || '1rem'};
+position: relative;
+text-shadow: 1px 1px 0.5px #000;
+
+:before{
+    position:absolute ;
+    content: '';
+    width: 3rem ;
+    height: 3px ;
+    bottom: -3px;
+    background:var(--primary) ;
+}
 `
