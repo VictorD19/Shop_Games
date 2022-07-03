383f35e59ef9f431151acf6300b07370409ac64e
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-07-03 01:14:21 -0300

    :ok_hand: styled: Add position toasts

--- Frontend/src/Components/Toast/index.jsx
+++ Frontend/src/Components/Toast/index.jsx
@@ -3,8 +3,8 @@ import { ToastContainerMain } from "./toash.styled";
 
 export const ToastNotification = ({ show, close, message, type }) => {
   return (
-    <ToastContainerMain>
-      <Toast onClose={close} show={show} delay={3000} autohide type={type}>
+    <ToastContainerMain position='top-end'>
+      <Toast onClose={close} show={show} delay={3000} autohide type={type} >
         <Toast.Body>{message}</Toast.Body>
       </Toast>
     </ToastContainerMain>
